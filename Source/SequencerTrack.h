61a120faf55db90f8224bc4c68b23e85a3bfc018
Bennet Leff <bennetleff@gmail.com>
2018-11-12 13:36:32 -0600

    More clean up; Still uncertain on fix for unclickable buttons

--- Source/SequencerTrack.h
+++ Source/SequencerTrack.h
@@ -34,18 +34,10 @@ class SequencerButton : public ChangeBroadcaster, public TextButton
 public:
 	SequencerButton()
 		: on_colour_(Colours::yellow), off_colour_(Colours::greenyellow), triggered_colour_(Colours::tomato),
-			is_on_(false) { } 
-
-	SequencerButton(const Colour& on_colour, const Colour& off_colour, const Colour& trigger_colour)
-		: on_colour_(on_colour), off_colour_(off_colour), triggered_colour_(trigger_colour),
 			is_on_(false) { }
 
-	void attach_sample_source(ChangeListener& sample_source);
 	void toggle_color();
 	void trigger_sequencer_colour();
-
-//	void resized() override;
-//	void paint(Graphics& g) override;
 
 	bool is_on_;
 
@@ -59,24 +51,14 @@ class Sequencer;
 
 /*
  * A SequencerTrack is a small abstraction over an array that represents a row of
- * sequencer steps/button triggers. 
- * Should almost certainly be made into a GUI element/Component.
- * As a ChangeListener, each SequencerTrack is notified to update each time the "global"
- * Sequencer updates. The global Sequencer acts as a Broadcaster which sends messages to
- * its recipient SequencerTrack(s) and other Listeners.
+ * sequencer steps/button triggers.
  */
-class SequencerTrack : public ChangeListener,
-                       public Component
+class SequencerTrack : public Component
 {
 public:
 	SequencerTrack();
 
-	void changeListenerCallback(ChangeBroadcaster* source) override;
-	void resized() override;
-
 	bool is_step_on(uint32_t step) { return sequencer_buttons_.at(step)->is_on_; }
-
-	void attach_sample(ChangeListener& sample_source);
 
 	/*
 	 * position_triggers offsets a SequencerTrack's buttons.
@@ -87,5 +69,4 @@ public:
 	void update(int step);
 
 	std::array<std::unique_ptr<SequencerButton>, NUM_SEQUENCER_STEPS> sequencer_buttons_;
-
 };
